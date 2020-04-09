9102fc012bac6ee45a12ec0c734b5a95bc0b3291
tomuhenry <tomusangehenry02@gmail.com>
2020-04-09 17:37:35 +0300

    challenge 1 code

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,18 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+  var reportedCases = data.reportedCases;
+  return {
+    data: data,
+    impact: covidImpact(reportedCases, 10),
+    severeImpact: covidImpact(reportedCases, 50)
+  };
+};
+
+const covidImpact = (reportedCases, people) => {
+  var currentlyInfected = reportedCases * people;
+  return {
+    currentlyInfected: currentlyInfected,
+    infectionsByRequestedTime: currentlyInfected * 1024
+  };
+};
 
 export default covid19ImpactEstimator;
