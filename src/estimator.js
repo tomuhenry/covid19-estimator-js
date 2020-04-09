242b44ec54fbea0f80453e138647893acb0bb076
tomuhenry <tomusangehenry02@gmail.com>
2020-04-09 17:37:35 +0300

    put covidImpact function in different file

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,9 @@
-const covid19ImpactEstimator = (data) => data;
+/* eslint-disable linebreak-style */
+import covidImpact from './covidImpact';
 
+const covid19ImpactEstimator = (data) => ({
+  data,
+  impact: covidImpact(data, 10),
+  severeImpact: covidImpact(data, 50)
+});
 export default covid19ImpactEstimator;
