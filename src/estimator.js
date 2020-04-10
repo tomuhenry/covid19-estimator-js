e6a00454c0806a20d749b60c2adcc158feeed37e
tomuhenry <tomusangehenry02@gmail.com>
2020-04-10 11:58:00 +0300

    challenge 3 calculations

--- src/estimator.js
+++ src/estimator.js
@@ -1,20 +1,32 @@
 const covidImpact = (reportedCases, people, beds) => {
   const currentlyInfected = reportedCases * people;
-  const infectionsByRequestedTime = currentlyInfected * 1024;
-  const severeCasesByRequestedTime = Math.round(
+  const infectionsByRequestedTime = currentlyInfected * 1024; // considering 30 days
+  const severeCasesByRequestedTime = Math.floor(
     infectionsByRequestedTime * 0.15
   );
-  const bedCapacity = Math.round(beds * 0.95);
-  const availableBedsSevere = Math.round(bedCapacity * 0.35);
+  const bedCapacity = Math.floor(beds * 0.95);
+  const availableBedsSevere = Math.floor(bedCapacity * 0.35);
 
-  const hospitalBedsByRequestedTime =
-    availableBedsSevere - severeCasesByRequestedTime;
+  const bedsByTime = availableBedsSevere - severeCasesByRequestedTime;
+
+  const casesForICUByRequestedTime = Math.floor(
+    infectionsByRequestedTime * 0.05
+  );
+  const casesForVentilatorsByRequestedTime = Math.floor(
+    infectionsByRequestedTime * 0.02
+  );
+  const dollarsInFlight = Math.floor(
+    (infectionsByRequestedTime * 0.65 * 1.5) / 30 // $$ spent daily by majority 65%
+  );
 
   return {
     currentlyInfected,
     infectionsByRequestedTime,
     severeCasesByRequestedTime,
-    hospitalBedsByRequestedTime
+    hospitalBedsByRequestedTime: bedsByTime,
+    casesForICUByRequestedTime,
+    casesForVentilatorsByRequestedTime,
+    dollarsInFlight
   };
 };
 
@@ -22,7 +34,7 @@ const covid19ImpactEstimator = (data) => {
   const reportedCases = data.reportedCases;
   const beds = data.totalHospitalBeds;
   return {
-    data: data,
+    data,
     impact: covidImpact(reportedCases, 10, beds),
     severeImpact: covidImpact(reportedCases, 50, beds)
   };
