1827975b71ffdfe268a4325a8f45e6f1e57fb179
tomuhenry <tomusangehenry02@gmail.com>
2020-04-14 00:58:40 +0300

    frontend UI

--- src/estimator.js
+++ src/estimator.js
@@ -1,5 +1,59 @@
 /* eslint-disable linebreak-style */
-import covidImpact from './covidImpact';
+
+const covidImpact = (data, people) => {
+  const {
+    reportedCases,
+    periodType,
+    totalHospitalBeds,
+    region: { avgDailyIncomeInUSD, avgDailyIncomePopulation } = {}
+  } = data;
+  let { timeToElapse } = data;
+  const currentlyInfected = reportedCases * people;
+
+  if (periodType === 'days') {
+    timeToElapse *= 1;
+  } else if (periodType === 'weeks') {
+    timeToElapse *= 7;
+  } else if (periodType === 'months') {
+    timeToElapse *= 30;
+  }
+  const factor = Math.trunc(timeToElapse / 3);
+  const infectionsByRequestedTime = currentlyInfected * 2 ** factor;
+  const severeCasesByRequestedTime = Math.trunc(
+    infectionsByRequestedTime * 0.15
+  );
+
+  const availableBeds = totalHospitalBeds * 0.35;
+
+  const hospitalBedsByRequestedTime = Math.trunc(
+    availableBeds - severeCasesByRequestedTime
+  );
+
+  const casesForICUByRequestedTime = Math.trunc(
+    infectionsByRequestedTime * 0.05
+  );
+
+  const casesForVentilatorsByRequestedTime = Math.trunc(
+    infectionsByRequestedTime * 0.02
+  );
+
+  const dollarsInFlight = Math.trunc(
+    (infectionsByRequestedTime
+      * avgDailyIncomePopulation
+      * avgDailyIncomeInUSD)
+      / timeToElapse
+  );
+
+  return {
+    currentlyInfected,
+    infectionsByRequestedTime,
+    severeCasesByRequestedTime,
+    hospitalBedsByRequestedTime,
+    casesForICUByRequestedTime,
+    casesForVentilatorsByRequestedTime,
+    dollarsInFlight
+  };
+};
 
 const covid19ImpactEstimator = (data) => ({
   data,
